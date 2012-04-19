3eb1f7825937d9a97e2a707be580bd6360332f14
Carl Allen <carl@allenofmn.com>
2012-04-19 01:53:53 -0700

    Real poltergeist tests

--- spec/requests/questions_spec.rb
+++ spec/requests/questions_spec.rb
@@ -1,6 +1,27 @@
 require 'spec_helper'
 
 describe "Questions" do
+  describe "GET /questions/new" do
+    it "should show multi choice options when selected", :js => true do 
+      visit "/questions/new"
+      find("#question_type_multiplechoicequestion").click()
+      find("#multi_choice_options").should be_visible
+      find("#true_false_options").should_not be_visible
+      click_button("Create Question") #submit form with invalid data
+      find("#multi_choice_options").should be_visible
+      find("#true_false_options").should_not be_visible
+    end
+    it "should show true false options when selected", :js => true do 
+      visit "/questions/new"
+      find("#question_type_truefalsequestion").click()
+      find("#multi_choice_options").should_not be_visible
+      find("#true_false_options").should be_visible
+      click_button("Create Question") #submit form with invalid data
+      find("#multi_choice_options").should_not be_visible
+      find("#true_false_options").should be_visible
+    end
+  end
+  
   describe "GET /questions" do
     it "should redirect to login if unauthenticated" do
       visit questions_path
