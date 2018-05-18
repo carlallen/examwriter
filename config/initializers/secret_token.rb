b5f4fa27fd93e02ffa31ea7def1089e5772b6dfd
Eddy Kim <eddyhkim@gmail.com>
2018-05-18 07:29:04 -0700

    Some warnings to avoid false-positives for security issues.

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,5 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Examwriter::Application.config.secret_token = '1e0a78f0a09aff779960e440d3dd3831acc284fd9328b0618a30bfcb1e750d4336938f520a7d4911fc008ddec11b1628e20a38e1da4f2574734223a01626f410'
+Examwriter::Application.config.secret_token = ENV['THIS_IS_A_SAMPLE_APP']
+
